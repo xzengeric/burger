d5f4c9a9be6bbe00ebc87ad5be3a8bfa62e4d485
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-03-14 19:21:38 -0700

    done

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Beef Burgers');
+INSERT INTO burgers (burger_name) VALUES ('Portobello Mushroom Burgers');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burgers');
+
