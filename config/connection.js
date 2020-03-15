5c4c2046ce6d8828cc1a092e224d4b17ca5e70c8
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-03-14 19:36:13 -0700

    Creating JWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+
+// Make connection.
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,8 +14,9 @@ var connection = mysql.createConnection({
   password: "6yq2wwaw",
   database: "burgers_db"
 });
+};
 
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,6 +24,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
